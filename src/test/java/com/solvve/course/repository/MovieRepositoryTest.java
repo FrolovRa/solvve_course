a049e38e3eb16b5a6f834d4bc6a9d2497602fa44
frolov <frorom@icloud.com>
2020-03-09 20:01:39 +0300

    Home-work-16. Fixed checkstyle warnings. Added getIdsOfAllMovies method and test for it.

--- src/test/java/com/solvve/course/repository/MovieRepositoryTest.java
+++ src/test/java/com/solvve/course/repository/MovieRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.solvve.course.repository;
 
 import com.solvve.course.domain.Movie;
+import com.solvve.course.util.TestUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -21,11 +28,25 @@ public class MovieRepositoryTest {
     @Autowired
     private MovieRepository movieRepository;
 
+    @Autowired
+    private TestUtils utils;
+
     @Test
     public void testSave() {
         Movie movie = new Movie();
         Movie movieFromDb = movieRepository.save(movie);
         assertNotNull(movieFromDb);
         assertTrue(movieRepository.findById(movieFromDb.getId()).isPresent());
+    }
+
+    @Test
+    public void testGetAllMoviesIds() {
+        Set<UUID> expectedIds = new HashSet<>();
+        expectedIds.add(utils.getMovieFromDb().getId());
+        expectedIds.add(utils.getMovieFromDb().getId());
+
+        utils.inTransaction(() -> {
+            Assert.assertEquals(expectedIds, movieRepository.getIdsOfAllMovies().collect(Collectors.toSet()));
+        });
     }
 }
